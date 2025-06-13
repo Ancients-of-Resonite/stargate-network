41e1b77476d1010bfffe5c2ca09021c8d8e4f215
reality-exe <reality@rxserver.net>
2025-06-13 22:51:45 +0000

    add utilities

---
+++ src/utils/pocketbase.ts
@@ -0,0 +1,11 @@
+import PocketBase, { RecordService } from "pocketbase";
+import { Stargate } from "../types/stargate.ts";
+import { User } from "../types/user.ts";
+
+interface TPB extends PocketBase {
+  collection(idOrName: string): RecordService;
+  collection(idOrName: "stargates"): RecordService<Stargate>;
+  collection(idOrName: "users"): RecordService<User>;
+}
+
+export const pb = new PocketBase() as TPB;
