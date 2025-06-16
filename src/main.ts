ab0704cd5d6c9a68848554629d1097650d7bdc01
reality-exe <reality@rxserver.net>
2025-06-16 03:25:15 +0000

    update main.ts
    Added env checks
    Added pocketbase db login
    Added switch case for message types
    Added a quick socket closed log

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
 import { log } from "./utils/log.ts";
 
+import { MessageType } from "./types/messageTypes.ts";
+import dialRequest from "./handlers/dialRequest.ts";
+import { pb } from "./utils/pocketbase.ts";
+import { green } from "colors";
+
+if (!Deno.env.get("PB_ENDPOINT")) {
+  log.fatal("Please include PB_ENDPOINT as an environment variable");
+  Deno.exit(20);
+}
+
+if (!Deno.env.get("PB_EMAIL") || !Deno.env.get("PB_PASSWORD")) {
+  log.fatal(
+    "Please include an PB_EMAIL and PB_PASSWORD environment variable for the pocketbase database",
+  );
+  Deno.exit(20);
+}
+
+pb.admins.authWithPassword(
+  Deno.env.get("PB_EMAIL")!,
+  Deno.env.get("PB_PASSWORD")!,
+);
+
 Deno.serve((req, info) => {
   if (req.headers.get("upgrade") != "websocket") {
     return new Response(null, { status: 501 });
@@ -13,8 +35,32 @@ Deno.serve((req, info) => {
     );
   });
 
-  socket.addEventListener("message", (message) => {
-    log.info(message);
+  socket.addEventListener("message", (event) => {
+    if (event.data.startsWith("IDC")) return;
+
+    const data = JSON.parse(event.data);
+
+    switch (data.type) {
+      case MessageType.DialRequest:
+        log.info(
+          `Client ${
+            green(info.remoteAddr.hostname + ":" + info.remoteAddr.port)
+          } has requested to dial.`,
+        );
+        dialRequest({ data, socket });
+        break;
+      default:
+        socket.send("this type is not a thing");
+        break;
+    }
+  });
+
+  socket.addEventListener("close", (ev) => {
+    log.info(
+      `Client ${
+        green(info.remoteAddr.hostname + ":" + info.remoteAddr.port)
+      } has disconnected from the network.`,
+    );
   });
 
   return response;
