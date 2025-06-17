73188b49f81c4e2b36fe748b698c506833c2e15b
reality-exe <reality@rxserver.net>
2025-06-16 21:58:16 -0400

    session class stuff
    i still dont know if what im doing is good or not

--- src/types/session.ts
+++ src/types/session.ts
@@ -1,5 +1,28 @@
-declare class Session {
+export interface Session {
   gate_address: string;
   gate_code: string;
+  gate_status: string;
+  // Format: "<ip>:<port>"
+  remote: string;
+  connected_gate: {
+    connected: boolean;
+    gate_address?: string;
+    gate_code?: string;
+  };
 }
-export default Session;
+
+export class Sessions {
+  private sessions: Session[];
+
+  constructor() {
+    this.sessions = [];
+  }
+
+  public getSession(remote: string): Session | undefined {
+    return this.sessions.find((v) => v.remote == remote);
+  }
+
+  public pushSession(session: Session) {
+    this.sessions.push(session);
+  }
+}
