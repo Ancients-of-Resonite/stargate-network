2c5149202906faeeb37157973c3196e9566ea122
reality-exe <reality@rxserver.net>
2025-06-17 16:37:08 -0400

    start handling a dial request

--- src/handlers/dialRequest.ts
+++ src/handlers/dialRequest.ts
@@ -1,13 +1,31 @@
 import { DialRequest } from "@/types/messageTypes.ts";
 import { pb } from "../utils/pocketbase.ts";
+import { sessions } from "../main.ts";
 
 export default async function dialRequest(
-  { data, socket }: { data: DialRequest; socket: WebSocket },
+  { data, socket, remote }: {
+    data: DialRequest;
+    socket: WebSocket;
+    remote: string;
+  },
 ) {
   const address = data.gate_address.slice(0, 6);
   const code = data.gate_address.slice(6, 8);
-  const to_gate = await pb.collection("stargates").getFirstListItem(
-    `gate_address="${address}"`,
-  );
-  socket.send("dial request received\n" + data);
+  const session = sessions.getSession(remote);
+
+  if (session) {
+    if (address == session.gate_address) {
+      socket.send("CSDialCheck:403");
+      return;
+    }
+
+    try {
+      const gate = await pb.collection("stargates").getFirstListItem(
+        `gate_address = "${address}"`,
+      );
+    } catch {
+      socket.send("CSDialCheck:404");
+    }
+  }
+  socket.send("dial request received\n" + address + +" " + code);
 }
