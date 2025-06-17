404eb776c4663f250e44dc5ee15e794b0dc03023
reality-exe <reality@rxserver.net>
2025-06-17 13:13:31 +0000

    create new session

--- src/handlers/requestAddress.ts
+++ src/handlers/requestAddress.ts
@@ -2,6 +2,7 @@ import { cyan } from "colors";
 import { RequestAddress } from "@/types/messageTypes.ts";
 import { log } from "@/utils/log.ts";
 import { pb } from "@/utils/pocketbase.ts";
+import { sessions } from "../main.ts";
 
 export default async function requestAddress(
   { data, socket, remote }: {
@@ -15,10 +16,10 @@ export default async function requestAddress(
   });
 
   if (eg.length == 0) {
-    socket.send('{code:200,message:"Address accepted"}');
     log.info(
       `Accepted request for address ${data.gate_address}${data.gate_code}`,
     );
+
     const gate = await pb.collection("stargates").create({
       gate_address: data.gate_address,
       gate_code: data.gate_code,
@@ -40,6 +41,18 @@ export default async function requestAddress(
     socket.addEventListener("close", async () => {
       await unsub();
     });
+
+    sessions.pushSession({
+      gate_address: data.gate_address,
+      gate_code: data.gate_code,
+      remote: `${remote.hostname}:${remote.port}`,
+      gate_status: "IDLE",
+      connected_gate: {
+        connected: false,
+      },
+    });
+
+    socket.send('{code:200,message:"Address accepted"}');
   } else {
     log.info(
       `Denied request for address ${data.gate_address}${data.gate_code} for client ${
@@ -47,5 +60,7 @@ export default async function requestAddress(
       }. Address already taken`,
     );
     socket.send("403");
+
+    sessions.removeSession(`${remote.hostname}:${remote.port}`);
   }
 }
