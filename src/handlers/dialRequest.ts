db2a5983e9788783bf1cd6f06a388e3f00360165
reality-exe <reality@rxserver.net>
2025-06-18 23:39:36 -0400

    finished dialRequest

--- src/handlers/dialRequest.ts
+++ src/handlers/dialRequest.ts
@@ -1,6 +1,8 @@
 import { DialRequest } from "@/types/messageTypes.ts";
 import { pb } from "../utils/pocketbase.ts";
 import { sessions } from "../main.ts";
+import { log } from "../utils/log.ts";
+import { cyan, magenta, red } from "colors";
 
 export default async function dialRequest(
   { data, socket, remote }: {
@@ -12,6 +14,8 @@ export default async function dialRequest(
   const address = data.gate_address.slice(0, 6);
   const code = data.gate_address.slice(6, 8);
   const session = sessions.getSession(remote);
+
+  if (session?.connected_gate.state != "IDLE") return;
 
   if (session) {
     if (data.gate_address.length < 6) {
@@ -29,12 +33,51 @@ export default async function dialRequest(
         `gate_address = "${address}"`,
       );
 
+      if (data.gate_address.length > 6) {
+        if (gate.gate_code.startsWith(code)) {
+          socket.send("CSDialCheck:200");
+          socket.send(`CSDialedSessionURL:${gate.session_url}`);
+          sessions.updateSession(remote, gate);
+          await pb.collection("stargates").update(gate.id, {
+            gate_status: `INCOMING${data.gate_address.length + 1}`,
+          });
+          log.info(
+            `Dialout from ${
+              cyan(session.gate_address) + magenta(session.gate_code)
+            } to ${
+              cyan(gate.gate_address) + magenta(gate.gate_code)
+            } was successful.`,
+          );
+          return;
+        } else {
+          socket.send("CSDialCheck:302");
+          log.info(
+            `Dialout from ${
+              cyan(session.gate_address) + magenta(session.gate_code)
+            } to ${
+              cyan(gate.gate_address) + magenta(gate.gate_code)
+            } failed with code ${red("302")}.`,
+          );
+          return;
+        }
+      }
       if (gate.gate_code != session.gate_code) {
         socket.send("CSDialCheck:302");
         return;
       }
+
+      await pb.collection("stargates").update(gate.id, {
+        gate_status: "INCOMING7",
+      });
+      socket.send("CSDialCheck:200");
+      socket.send(`CSDialedSessionURL:${gate.session_url}`);
+      sessions.updateSession(remote, gate, "OUTGOING");
     } catch {
       socket.send("CSDialCheck:404");
+      log.info(
+        "Dialout failed, cannot dial your own address... You get a busy signal",
+      );
+      return;
     }
   }
   socket.send("dial request received\n" + address + +" " + code);
