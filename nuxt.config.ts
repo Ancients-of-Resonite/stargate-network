5d88ad04f2dc62f9eaa677319942b5b4edae6718
reality-exe <reality@rxserver.net>
2025-09-30 19:05:40 -0400

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["shadcn-nuxt", "@vueuse/nuxt", "@nuxtjs/color-mode"],
+  css: ["@/assets/css/main.css"],
+  colorMode: {
+    classSuffix: "",
+  },
+  shadcn: {
+    componentDir: "./components/ui",
+    prefix: "",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
