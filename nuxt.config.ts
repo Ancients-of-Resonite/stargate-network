6348d52e1b0f083fb2503befc4a39499fa5ad767
reality-exe <reality@rxserver.net>
2025-09-30 09:34:48 -0400

    initialize new nuxt project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,5 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  css: ["./assets/css/main.css"],
-  plugins: [
-    "./plugins/pocketbase.ts",
-  ],
-  modules: [
-    "shadcn-nuxt",
-    "@nuxtjs/color-mode",
-    "nuxt-particles",
-    "@nuxt/icon",
-    "@nuxt/image",
-  ],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  colorMode: {
-    classSuffix: "",
-  },
-  shadcn: {
-    prefix: "",
-    componentDir: "./components/ui",
-  },
-});
-
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
