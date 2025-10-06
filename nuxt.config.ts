4739d9cf19b8219ff140152e2f0763c5a27113bf
reality-exe <reality@rxserver.net>
2025-10-06 19:28:37 -0400

    update nuxt config
    added nuxt icon module
    fixed shadcn componentDir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,20 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "AoR - Stargate Network Dashboard"
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["shadcn-nuxt", "@vueuse/nuxt", "@nuxtjs/color-mode"],
+  modules: ["shadcn-nuxt", "@vueuse/nuxt", "@nuxtjs/color-mode", "@nuxt/icon"],
   css: ["@/assets/css/main.css"],
   colorMode: {
     classSuffix: "",
   },
   shadcn: {
-    componentDir: "./components/ui",
+    componentDir: "./app/components/ui",
     prefix: "",
   },
   vite: {
