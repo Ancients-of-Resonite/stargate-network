f3fb035c220a89feadd028519b2b39a7850a2b82
reality-exe <reality@rxserver.net>
2025-10-06 19:33:06 -0400

    add authSchema

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,6 +1,7 @@
 import { db } from "$packages/database/src/db";
 
 export * as schema from "$packages/database/src/schema";
+export * as authSchema from "$packages/database/src/auth-schema";
 
 export function useDrizzle() {
   return db;
