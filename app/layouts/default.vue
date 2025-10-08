736adbde02738c4446b34295164b55e4ef8d8a1d
reality-exe <reality@rxserver.net>
2025-10-07 23:16:00 -0400

    some testin with visuals

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { useStorage } from "@vueuse/core";
+const glyph_display = useStorage("glyph_display", "text");
+</script>
+
 <template>
   <header class="p-4 h-16 bg-sidebar flex items-center justify-between">
     <div class="flex gap-4 items-center">
@@ -9,10 +14,55 @@
       />
       <h1 class="text-2xl font-bold">Stargate Network</h1>
     </div>
-    
+
     <div class="flex gap-4 items-center">
+      <!-- <Button variant="ghost" as-child>
+        <a href="/login">Login</a>
+      </Button> -->
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" size="icon">
+            <Icon name="tabler:dots" size="24" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem>Admin Panel</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem disabled>My Account</DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger> Glyph Appearance </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup v-model="glyph_display">
+                <DropdownMenuRadioItem value="text">Text</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="mw"
+                  >Milky Way</DropdownMenuRadioItem
+                >
+                <DropdownMenuRadioItem value="pg"
+                  >Pegasus</DropdownMenuRadioItem
+                >
+                <DropdownMenuRadioItem value="un"
+                  >Universe</DropdownMenuRadioItem
+                ></DropdownMenuRadioGroup
+              >
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   </header>
   <slot />
-  <div class="absolute bottom-1 left-1 right-1 text-center text-3xl text-foreground/50 pointer-events-none font-ancient">Nou Ani Anquietas</div>
+  <div
+    class="absolute bottom-1 left-1 right-1 text-center text-3xl text-foreground/50 pointer-events-none font-ancient"
+  >
+    NOU ANI ANQUIETAS
+  </div>
 </template>
+
+<style>
+body {
+  background: url("/images/background.png") no-repeat center center fixed;
+  background-size: cover;
+}
+</style>
