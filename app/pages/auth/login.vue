14a2717c893211ca3208d363a1b8d908fe8269d8
reality-exe <reality@rxserver.net>
2025-10-10 10:19:37 -0400

    reformat

--- app/pages/auth/login.vue
+++ app/pages/auth/login.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: "empty",
+    layout: "empty",
 });
 
 import { useForm } from "vee-validate";
@@ -10,14 +10,14 @@ import * as z from "zod";
 const auth = useAuthClient();
 
 const formSchema = toTypedSchema(
-  z.object({
-    email: z.email("Invalid email address"),
-    password: z.string(),
-  })
+    z.object({
+        email: z.email("Invalid email address"),
+        password: z.string(),
+    }),
 );
 
 const form = useForm({
-  validationSchema: formSchema,
+    validationSchema: formSchema,
 });
 
 const onSubmit = form.handleSubmit(async (values) => {
@@ -45,58 +45,66 @@ const discordLogin = async (e: FormDataEvent) => {
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center p-4">
-    <Card class="mx-auto min-w-md">
-      <CardHeader>
-        <CardTitle class="text-xl">Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form @submit="onSubmit">
-          <div class="grid gap-4">
-            <FormField v-slot="{ componentField }" name="email">
-              <FormItem>
-                <div class="grid gap-2">
-                  <FormLabel for="email">Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="email"
-                      v-bind="componentField"
-                      type="email"
-                      placeholder="m@example.com"
-                      required
-                    />
-                  </FormControl>
-                  <FormMessage />
+    <div class="min-h-screen flex items-center justify-center p-4">
+        <Card class="mx-auto min-w-md">
+            <CardHeader>
+                <CardTitle class="text-xl">Login</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form @submit="onSubmit">
+                    <div class="grid gap-4">
+                        <FormField v-slot="{ componentField }" name="email">
+                            <FormItem>
+                                <div class="grid gap-2">
+                                    <FormLabel for="email">Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            id="email"
+                                            v-bind="componentField"
+                                            type="email"
+                                            placeholder="m@example.com"
+                                            required
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                            </FormItem>
+                        </FormField>
+                        <FormField v-slot="{ componentField }" name="password">
+                            <FormItem>
+                                <div class="grid gap-2">
+                                    <FormLabel for="password"
+                                        >Password</FormLabel
+                                    >
+                                    <FormControl>
+                                        <Input
+                                            id="password"
+                                            v-bind="componentField"
+                                            type="password"
+                                            required
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </div>
+                            </FormItem>
+                        </FormField>
+                        <Button type="submit" class="w-full">Login</Button>
+                        <Button
+                            variant="outline"
+                            class="w-full"
+                            @click="discordLogin"
+                        >
+                            Continue with Discord
+                        </Button>
+                    </div>
+                </form>
+                <div class="mt-4 text-center text-sm">
+                    Don't have an account?
+                    <a href="/auth/register" class="underline"
+                        >Register here!</a
+                    >
                 </div>
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="password">
-              <FormItem>
-                <div class="grid gap-2">
-                  <FormLabel for="password">Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="password"
-                      v-bind="componentField"
-                      type="password"
-                      required
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </div>
-              </FormItem>
-            </FormField>
-            <Button type="submit" class="w-full">Login</Button>
-            <Button variant="outline" class="w-full">
-              Continue with Discord
-            </Button>
-          </div>
-        </form>
-        <div class="mt-4 text-center text-sm">
-          Don't have an account?
-          <a href="/auth/register" class="underline">Register here!</a>
-        </div>
-      </CardContent>
-    </Card>
-  </div>
+            </CardContent>
+        </Card>
+    </div>
 </template>
