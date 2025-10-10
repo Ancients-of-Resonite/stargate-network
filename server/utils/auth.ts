07f8305c9bcc15b0e8c6b4f45288e6426095f65e
reality-exe <reality@rxserver.net>
2025-10-10 10:19:53 -0400

    add discord provider and additional tags field

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -6,6 +6,21 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  socialProviders: {
+    discord: {
+      enabled: true,
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    },
+  },
+  account: {
+    additionalFields: {
+      tags: {
+        type: "string[]",
+        default: ["user"],
+      },
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
