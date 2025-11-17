2b309728268c859b796067706703588f8c6b31d5
reality-exe <reality@rxserver.net>
2025-11-17 16:44:10 -0500

    move header stuff to a component to use as client only

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 useHead({
-  title: "AoR - Active Stargates",
+    title: "AoR - Active Stargates",
 });
 const { data: gates } = await useFetch("/api/stargates");
 </script>
 
 <template>
-  <div class="p-4 flex flex-wrap">
-    <ClientOnly>
-      <GateListItem v-for="gate in gates" :gate="gate" />
-    </ClientOnly>
-  </div>
+    <UserDropdown />
+    <div class="p-4 flex flex-wrap">
+        <GateListItem v-for="gate in gates" :gate="gate" />
+    </div>
 </template>
