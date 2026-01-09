a4516f310f8b041925442eae14270a4a0f55e2c0
reality-exe <reality@rxserver.net>
2026-01-09 09:26:31 -0500

    admin page stuff

--- app/components/UserDropdown.vue
+++ app/components/UserDropdown.vue
@@ -3,71 +3,66 @@ import { useStorage } from "@vueuse/core";
 const glyph_display = useStorage("glyph_display", "text");
 
 const { user } = await useAuthUser();
+const isAdmin = user?.tags.includes("admin");
 
 const logout = async () => {
-    const auth = useAuthClient();
-    await auth.signOut();
-    reloadNuxtApp();
+  const auth = useAuthClient();
+  await auth.signOut();
+  reloadNuxtApp();
 };
 </script>
 
 <template>
-    <header class="p-4 h-16 bg-sidebar flex items-center justify-between">
-        <div class="flex gap-4 items-center">
-            <img
-                src="/images/AoR_Chevron2.png"
-                alt="Stargate Network Logo"
-                width="45"
-                height="45"
-            />
-            <h1 class="text-2xl font-bold">Stargate Network</h1>
-        </div>
+  <header class="p-4 h-16 bg-sidebar flex items-center justify-between">
+    <div class="flex gap-4 items-center">
+      <img
+        src="/images/AoR_Chevron2.png"
+        alt="Stargate Network Logo"
+        width="45"
+        height="45"
+      />
+      <h1 class="text-2xl font-bold">Stargate Network</h1>
+    </div>
 
-        <div class="flex gap-4 items-center">
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Button variant="ghost" size="icon">
-                        <Icon name="tabler:dots" size="24" />
-                    </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                    <DropdownMenuItem disabled v-if="user"
-                        >My Account</DropdownMenuItem
-                    >
-                    <DropdownMenuItem v-if="!user" as="a" href="/auth/login"
-                        >Login</DropdownMenuItem
-                    >
-                    <DropdownMenuSeparator />
-                    <DropdownMenuSub>
-                        <DropdownMenuSubTrigger>
-                            Glyph Appearance
-                        </DropdownMenuSubTrigger>
-                        <DropdownMenuSubContent>
-                            <DropdownMenuRadioGroup v-model="glyph_display">
-                                <DropdownMenuRadioItem value="text"
-                                    >Text</DropdownMenuRadioItem
-                                >
-                                <DropdownMenuRadioItem value="mw"
-                                    >Milky Way</DropdownMenuRadioItem
-                                >
-                                <DropdownMenuRadioItem value="pg"
-                                    >Pegasus</DropdownMenuRadioItem
-                                >
-                                <DropdownMenuRadioItem value="un"
-                                    >Universe</DropdownMenuRadioItem
-                                ></DropdownMenuRadioGroup
-                            >
-                        </DropdownMenuSubContent>
-                    </DropdownMenuSub>
-                    <DropdownMenuSeparator v-if="user" />
-                    <DropdownMenuItem
-                        v-if="user"
-                        variant="destructive"
-                        @click="logout"
-                        >Logout</DropdownMenuItem
-                    >
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </div>
-    </header>
+    <div class="flex gap-4 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="ghost" size="icon">
+            <Icon name="tabler:dots" size="24" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem disabled v-if="user">My Account</DropdownMenuItem>
+          <DropdownMenuItem as-child v-if="isAdmin">
+            <a href="/admin/dashboard">Admin Dashboard</a>
+          </DropdownMenuItem>
+          <DropdownMenuItem v-if="!user" as="a" href="/auth/login"
+            >Login</DropdownMenuItem
+          >
+          <DropdownMenuSeparator />
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger> Glyph Appearance </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuRadioGroup v-model="glyph_display">
+                <DropdownMenuRadioItem value="text">Text</DropdownMenuRadioItem>
+                <DropdownMenuRadioItem value="mw"
+                  >Milky Way</DropdownMenuRadioItem
+                >
+                <DropdownMenuRadioItem value="pg"
+                  >Pegasus</DropdownMenuRadioItem
+                >
+                <DropdownMenuRadioItem value="un"
+                  >Universe</DropdownMenuRadioItem
+                ></DropdownMenuRadioGroup
+              >
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
+          <DropdownMenuSeparator v-if="user" />
+          <DropdownMenuItem v-if="user" variant="destructive" @click="logout"
+            >Logout</DropdownMenuItem
+          >
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  </header>
 </template>
