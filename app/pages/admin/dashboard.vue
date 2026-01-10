6e585a9bead990dfca87fb6df6e588c710084a44
reality-exe <reality@rxserver.net>
2026-01-10 18:40:58 +0000

    placeholder stuff

--- app/pages/admin/dashboard.vue
+++ app/pages/admin/dashboard.vue
@@ -4,3 +4,72 @@ definePageMeta({
   layout: "admin",
 });
 </script>
+
+<template>
+  <main class="flex flex-col gap-4 mt-4">
+    <div class="flex gap-4 w-full">
+      <Card class="flex-1">
+        <CardHeader>
+          <CardTitle>Active Stargates</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h1
+            class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
+          >
+            20
+          </h1>
+        </CardContent>
+      </Card>
+      <Card class="flex-1">
+        <CardHeader>
+          <CardTitle>Active MilkyWay Stargates</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h1
+            class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
+          >
+            20
+          </h1>
+        </CardContent>
+      </Card>
+      <Card class="flex-1">
+        <CardHeader>
+          <CardTitle>Active Pegasus Stargates</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h1
+            class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
+          >
+            20
+          </h1>
+        </CardContent>
+      </Card>
+
+      <Card class="flex-1">
+        <CardHeader>
+          <CardTitle>Active Universe Stargates</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h1
+            class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
+          >
+            20
+          </h1>
+        </CardContent>
+      </Card>
+
+      <Card class="flex-1">
+        <CardHeader>
+          <CardTitle>Active Dawn Stargates</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <h1
+            class="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
+          >
+            20
+          </h1>
+        </CardContent>
+      </Card>
+    </div>
+  </main>
+</template>
