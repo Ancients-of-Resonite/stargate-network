71c9c413e7cd6c2ba77b2c42d4bfb95790295260
reality-exe <reality@rxserver.net>
2026-01-18 22:01:39 -0500

    back to drizzle lol

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -2,24 +2,18 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema.ts";
 import * as authSchema from "./auth-schema.ts";
 import pg from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
-import {PrismaClient} from "../prisma/generated/prisma/client.ts"
+
+export { eq } from "drizzle-orm"
 
 const { Pool } = pg;
 
-// export const db = drizzle({
-//   client: new Pool({
-//     connectionString: process.env.DB_URL,
-//   }),
-//   schema: {
-//     ...schema,
-//     ...authSchema
-//   },
-// });
+export const db = drizzle({
+  client: new Pool({
+    connectionString: process.env.DB_URL,
+  }),
+  schema: {
+    ...schema,
+    ...authSchema
+  },
+});
 
-const connectionString = Bun.env.DB_URL;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({adapter})
-
-export {prisma}
