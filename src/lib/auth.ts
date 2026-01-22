d00405eb10648405a770643305c6d417e681c6b5
reality-exe <reality@rxserver.net>
2026-01-21 23:41:30 -0500

    better auth stuff

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { db } from "database/src/db"
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
+  socialProviders: {
+    discord: {
+      enabled: true,
+      clientId: Bun.env.DISCORD_CLIENT_ID!,
+      clientSecret: Bun.env.DISCORD_CLIENT_SECRET!
+    }
+  },
+  user: {
+    additionalFields: {
+      tags: {
+        type: "string[]",
+        required: true,
+        input: false,
+        defaultValue: []
+      }
+    }
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg"
+  })
+})
+
