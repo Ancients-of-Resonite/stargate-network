472cdb45c119b1bf89bf2bc98c1c12aa3d54f141
reality-exe <reality@rxserver.net>
2026-01-23 02:03:59 -0500

    add gate data

--- src/components/stargate-card.tsx
+++ src/components/stargate-card.tsx
@@ -1,8 +1,25 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { stargate } from "database/src/schema";
 import { Lock, Server, User } from "lucide-react";
-import { Card, CardHeader, CardTitle } from "../components/ui/card";
+import localFont from "next/font/local";
+import { useLocalStorage } from "usehooks-ts";
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
+
+const mw_font = localFont({
+  src: "../assets/sg_mw.ttf",
+});
+
+const pg_font = localFont({
+  src: "../assets/sg_pg.ttf",
+});
+
+const un_font = localFont({
+  src: "../assets/sg_uni.ttf",
+});
 
 export function StargateCard({ gate }: { gate: typeof stargate.$inferInsert }) {
+  const [glyphs, _] = useLocalStorage("glyph-display-type", "text");
   return (
     <Card className="w-[320px]">
       <CardHeader>
@@ -10,6 +27,36 @@ export function StargateCard({ gate }: { gate: typeof stargate.$inferInsert }) {
           {gate.is_headless ? <Server size={16} /> : <User size={16} />} {!gate.public_gate && <Lock size={16} />}{" "}
           {gate.session_name}
         </CardTitle>
+        <CardContent className="mt-2 space-y-2">
+          <div className="flex justify-between">
+            <p>Gate Address</p>
+            <p
+              className={cn(
+                glyphs === "text" && "font-inter",
+                glyphs === "mw" && `${mw_font.className}`,
+                glyphs === "pg" && `${pg_font.className} text-2xl`,
+                glyphs === "un" && `${un_font.className}`,
+                !gate.public_gate && "blur-sm hover:blur-none transition-all",
+              )}
+            >
+              {gate.gate_address}
+              <span className="text-primary">{gate.gate_code}</span>
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <p>Owner</p>
+            <p>
+              {gate.owner_name}
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <p>User Count</p>
+            <p>
+              {gate.active_users}/
+              <span className="text-primary">{gate.max_users}</span>
+            </p>
+          </div>
+        </CardContent>
       </CardHeader>
     </Card>
   );
