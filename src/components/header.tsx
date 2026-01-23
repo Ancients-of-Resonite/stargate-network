050602c27e73ce0fcc041746e877b25373c9cab1
reality-exe <reality@rxserver.net>
2026-01-23 05:01:28 +0000

    dynamically show/hide buttons based on session status

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,7 @@
+import { auth } from "@/lib/auth";
+import { DropdownMenuArrow } from "@radix-ui/react-dropdown-menu";
 import { Ellipsis } from "lucide-react";
+import { headers } from "next/headers";
 import Image from "next/image";
 import { GlyphSelector } from "./glyph-selector";
 import { Button } from "./ui/button";
@@ -13,7 +16,9 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 
-export function Header() {
+export async function Header() {
+  const reqHead = await headers();
+  const session = await auth.api.getSession({ headers: reqHead });
   return (
     <div className="p-4 h-16 flex items-center bg-slate-400 dark:bg-neutral-900">
       <div className="flex-1 flex gap-2 items-center">
@@ -21,24 +26,38 @@ export function Header() {
         <p className="text-xl">Ancients of Resonite</p>
       </div>
       <DropdownMenu>
-        <DropdownMenuTrigger asChild>
+        <DropdownMenuTrigger className="w-42" asChild>
           <Button size="icon" variant="ghost">
             <Ellipsis />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
+        <DropdownMenuContent align="end">
+          <DropdownMenuArrow />
           <DropdownMenuGroup>
-            <DropdownMenuItem>My Account</DropdownMenuItem>
+            {session && <DropdownMenuItem>My Account</DropdownMenuItem>}
+            {!session && (
+              <DropdownMenuItem asChild>
+                <a href="/auth">Login</a>
+              </DropdownMenuItem>
+            )}
             <GlyphSelector />
           </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem>Admin Dashboard</DropdownMenuItem>
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuGroup>
-            <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
-          </DropdownMenuGroup>
+          {session?.user.tags.includes("admin") && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Admin Dashboard</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </>
+          )}
+          {session && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem variant="destructive">Logout</DropdownMenuItem>
+              </DropdownMenuGroup>
+            </>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
