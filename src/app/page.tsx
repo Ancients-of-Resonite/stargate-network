1c927ef83d75f0bc47332791e5a59159486f1fed
reality-exe <reality@rxserver.net>
2026-01-22 20:01:53 -0500

    disable fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Header } from "@/components/header";
 
 export default async function Home() {
-  const data = await fetch("/api/stargates");
-  const stargates = data.json();
+  // const data = await fetch("/api/stargates");
+  // const stargates = data.json();
 
   return (
     <div className="bg-zinc-50 font-sans dark:bg-black">
