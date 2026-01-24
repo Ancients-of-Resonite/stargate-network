e89d92fc3deea2c75f63b4735f4847db53cc09e4
reality-exe <reality@rxserver.net>
2026-01-24 06:48:23 +0000

    add actions column and colourize the status codes

--- src/components/admin/tables/gatelogs/columns.tsx
+++ src/components/admin/tables/gatelogs/columns.tsx
@@ -1,8 +1,21 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 import { gateLog } from "database/src/schema";
+import { ArrowRight, MoreHorizontal } from "lucide-react";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const Time = dynamic(() => import("./time"), { ssr: false });
 
 export const columns: ColumnDef<typeof gateLog.$inferSelect>[] = [
   {
@@ -13,7 +26,25 @@ export const columns: ColumnDef<typeof gateLog.$inferSelect>[] = [
   {
     accessorKey: "status",
     header: "Status",
-    cell: (({ row }) => <Badge variant="ghost">{row.getValue("type")}</Badge>),
+    cell: (({ row }) => {
+      const status = row.getValue("status") as number;
+
+      return (
+        <Badge
+          className={cn(
+            status == 200 && "bg-green-400/50",
+            status == 418 && "bg-green-400/50",
+            status == 403 && "bg-amber-500/50",
+            status == 302 && "bg-amber-500/50",
+            status == 404 && "bg-red-400/50",
+            status == 500 && "bg-red-800",
+          )}
+          variant="secondary"
+        >
+          {status}
+        </Badge>
+      );
+    }),
   },
   {
     accessorKey: "remote",
@@ -22,5 +53,29 @@ export const columns: ColumnDef<typeof gateLog.$inferSelect>[] = [
   {
     accessorKey: "created",
     header: "Created",
+    cell: ({ row }) => {
+      const date = row.getValue("created") as Date;
+      return <Time date={date} />;
+    },
+  },
+  {
+    accessorKey: "actions",
+    cell: ({ row }) => {
+      const log = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              Details
+              <ArrowRight size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel></DropdownMenuLabel>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
