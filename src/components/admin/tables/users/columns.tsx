22b2b5d783461af89ba83012f7bf9d58a85824f1
reality-exe <reality@rxserver.net>
2026-02-25 22:32:41 -0500

    add actions to users column

--- src/components/admin/tables/users/columns.tsx
+++ src/components/admin/tables/users/columns.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
 import { user } from "database/src/auth-schema";
+import { MoreHorizontal } from "lucide-react";
 
 export const columns: ColumnDef<typeof user.$inferSelect>[] = [
   {
@@ -18,7 +26,29 @@ export const columns: ColumnDef<typeof user.$inferSelect>[] = [
     header: "Tags",
     cell: (({ row }) => {
       const tags: string[] = row.getValue("tags");
-      return tags.map(tag => <Badge key={tag}>{tag}</Badge>);
+      return <div className="flex gap-2">{tags.map(tag => <Badge key={tag}>{tag}</Badge>)}</div>;
     }),
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+    cell: ({ row }) => {
+      const gate = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="h-8 w-8" asChild>
+            <Button size="icon" variant="ghost">
+              <MoreHorizontal />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem asChild>
+              <a href={`/admin/users/edit/${gate.id}`}>Edit Gate</a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
