c0f252fad9685190b0d0dd7f63f070baf76f0128
reality-exe <reality@rxserver.net>
2026-02-25 22:31:34 -0500

    make public/private/total gate count as dedicated cards

--- src/app/admin/stargates/page.tsx
+++ src/app/admin/stargates/page.tsx
@@ -1,22 +1,22 @@
 import { DataTable } from "@/components/admin/tables/data-table";
 import { columns } from "@/components/admin/tables/stargate/columns";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Kbd } from "@/components/ui/kbd";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { db } from "database/src/db";
 import { stargate } from "database/src/schema";
 
 export default async function AdminStargates() {
   const stargates = await db.select().from(stargate);
+
   return (
     <main className="space-y-4">
       <Card>
         <CardContent className="flex justify-between items-center">
-          <SidebarTrigger />{" "}
-          <p>
-            /admin/stargates [{stargates.length} total active stargate | {stargates.filter(v => v.public_gate).length}
-            {" "}
-            total public gates | {stargates.filter(v => !v.public_gate).length} total private gates]
-          </p>
+          <div className="flex gap-2 items-center">
+            <SidebarTrigger />
+            <Kbd>/admin/stargates</Kbd>
+          </div>
         </CardContent>
       </Card>
       <div className="flex gap-4 flex-wrap">
@@ -43,6 +43,26 @@ export default async function AdminStargates() {
             <CardTitle>Dawn Stargates</CardTitle>
           </CardHeader>
           <CardContent>{stargates.filter(v => v.gate_code === "R@").length}</CardContent>
+        </Card>
+      </div>
+      <div className="flex gap-4 flex-wrap">
+        <Card className="flex-1 min-w-52">
+          <CardHeader>
+            <CardTitle>Total Gates</CardTitle>
+          </CardHeader>
+          <CardContent>{stargates.length}</CardContent>
+        </Card>
+        <Card className="flex-1 min-w-52">
+          <CardHeader>
+            <CardTitle>Public Gates</CardTitle>
+          </CardHeader>
+          <CardContent>{stargates.filter(v => v.public_gate).length}</CardContent>
+        </Card>
+        <Card className="flex-1 min-w-52">
+          <CardHeader>
+            <CardTitle>Private Gates</CardTitle>
+          </CardHeader>
+          <CardContent>{stargates.filter(v => !v.public_gate).length}</CardContent>
         </Card>
       </div>
       <Card>
