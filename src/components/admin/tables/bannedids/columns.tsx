70809bb2fea24f07f9cb16ed83080330d2f82b7c
reality-exe <reality@rxserver.net>
2026-02-27 14:45:50 -0500

    make delete button work

--- src/components/admin/tables/bannedids/columns.tsx
+++ src/components/admin/tables/bannedids/columns.tsx
@@ -1,16 +1,40 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { AlertDialogContent } from "@radix-ui/react-alert-dialog";
 import { ColumnDef } from "@tanstack/react-table";
 import { bannedIds } from "database/src/schema";
-import { MoreHorizontal, Trash } from "lucide-react";
+import { MoreHorizontal, Trash, Trash2Icon } from "lucide-react";
+import { refresh } from "next/cache";
+import { useRouter } from "next/router";
+import { unban } from "./unban";
 
 export const columns: ColumnDef<typeof bannedIds.$inferSelect>[] = [
   {
@@ -24,13 +48,38 @@ export const columns: ColumnDef<typeof bannedIds.$inferSelect>[] = [
   {
     accessorKey: "actions",
     header: "",
-    cell: ({ row }) => {
-      const gate = row.original;
+    cell: ({ row, table }) => {
+      const user = row.original;
 
       return (
-        <Button variant="destructive" size="icon">
-          <Trash />
-        </Button>
+        <Dialog>
+          <DialogTrigger>
+            <Button variant="destructive">
+              <Trash2Icon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure?</DialogTitle>
+            </DialogHeader>
+            Are you sure you want to unban this user?
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="secondary">Cancel</Button>
+              </DialogClose>
+              <DialogClose asChild>
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    unban(user);
+                  }}
+                >
+                  Yes, unban
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       );
     },
   },
