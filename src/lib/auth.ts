c25dbd0f8e64a798e31e200a269bbfda8bb9e889
reality-exe <reality@rxserver.net>
2026-02-28 21:20:59 -0500

    add api keys plugin

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,12 @@ import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { nextCookies } from "better-auth/next-js"
 import { db } from "database/src/db"
+import {apiKey} from "better-auth/plugins"
 
 export const auth = betterAuth({
   plugins: [
-    nextCookies()
+    nextCookies(),
+    apiKey()
   ],
   emailAndPassword: {
     enabled: true,
