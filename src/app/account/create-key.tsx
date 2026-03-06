059d6e33f15d019444fe2c3a9bf602871ce13db1
reality-exe <reality@rxserver.net>
2026-03-06 17:45:17 +0000

    update create-key.tsx
    added an extra dialog to show the generated key

--- src/app/account/create-key.tsx
+++ src/app/account/create-key.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/dialog";
 import { Field, FieldContent, FieldDescription, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { Kbd } from "@/components/ui/kbd";
 import { reload } from "@/lib/actions";
 import { authClient } from "@/lib/auth-client";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -28,11 +29,16 @@ const formSchema = z.object({
 
 export default function CreateKey() {
   const [open, setOpen] = useState(false);
+  const [created, setCreated] = useState(false);
+  const [key, setKey] = useState("");
 
   const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
   });
 
   async function onSubmit(data: z.infer<typeof formSchema>) {
@@ -42,61 +48,84 @@ export default function CreateKey() {
       prefix: "sgn_",
       name: data.name,
     }).then((key) => {
-      toast(`Created Key: ${key.data?.key}`, {
-        position: "top-center",
-        description: "You will have to manually ",
-        action: (
-          <Button
-            onClick={() => {
-              navigator.clipboard.writeText(key.data?.key ?? "");
-            }}
-          >
-            Copy API Key
-          </Button>
-        ),
-      });
+      setKey(key.data!.key);
+      setCreated(true);
     }).finally(() => {
       setOpen(false);
+      form.reset();
       reload("/account");
       setLoading(false);
     });
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button>Create New API Key</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Create API Key</DialogTitle>
-          <DialogDescription>Create an API key to interact with the stargate network's API</DialogDescription>
-        </DialogHeader>
-        <form id="key-form" onSubmit={form.handleSubmit(onSubmit)}>
-          <FieldGroup>
-            <Controller
-              control={form.control}
-              name="name"
-              render={({ field, fieldState }) => (
-                <Field data-invalid={fieldState.invalid}>
-                  <FieldContent>
-                    <FieldLabel htmlFor="apiname">Key Name</FieldLabel>
-                    <FieldDescription>A memorable identifier for this API key</FieldDescription>
-                  </FieldContent>
-                  <Input {...field} id="apiname" aria-invalid={fieldState.invalid} />
-                  {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-                </Field>
-              )}
-            />
-          </FieldGroup>
-        </form>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </DialogClose>
-          <Button form="key-form">Create Key</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+    <>
+      <Dialog open={created} onOpenChange={setCreated}>
+        <DialogContent className="min-w-[690px]">
+          <DialogHeader>
+            <DialogTitle>API Key Created</DialogTitle>
+            <DialogDescription>
+              Please copy this key and save it somewhere safe. You will not be able to regain this key after closing
+              this menu.
+              <br />
+              <br />
+              <Kbd className="text-sm select-all">{key}</Kbd>
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button
+              size="lg"
+              className="w-full"
+              onClick={() => {
+                navigator.clipboard.writeText(key).then(() => {
+                  toast.success("Copied to clipboard");
+                  setCreated(false);
+                  setKey("");
+                }).catch(() => {
+                  toast.error("Failed to copy to clipboard");
+                });
+              }}
+            >
+              Copy key
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="w-full" variant="outline">Create New API Key</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create API Key</DialogTitle>
+            <DialogDescription>Create an API key to interact with the stargate network's API</DialogDescription>
+          </DialogHeader>
+          <form id="key-form" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                control={form.control}
+                name="name"
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldContent>
+                      <FieldLabel htmlFor="apiname">Key Name</FieldLabel>
+                      <FieldDescription>A memorable identifier for this API key</FieldDescription>
+                    </FieldContent>
+                    <Input {...field} id="apiname" aria-invalid={fieldState.invalid} />
+                    {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="secondary">Cancel</Button>
+            </DialogClose>
+            <Button form="key-form">Create Key</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
