6bacd7fa6c0772460eb4d29b0e98fa170bef51d3
reality-exe <reality@rxserver.net>
2026-03-12 01:21:59 +0000

    create server start script

---
+++ start-server.sh
@@ -0,0 +1,9 @@
+cd packages/database
+
+if bun run db:migrate | grep -q 'migrations applied successfully!'; then
+    echo Successfully migrated schema to database... Starting websocket
+
+    cd ../../apps/sgn-socket
+
+    bun run src/main.ts
+fi
