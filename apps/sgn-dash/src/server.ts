b0183049ac0395304e076c5d70af81cfc9c984ce
reality-exe <reality@uninetwork.net>
2026-04-14 23:50:28 +0000

    Check apikey on connection, instead of every update
    This should reduce the amount of API rate limiting lol idk why i didnt
    do this in the first place

--- apps/sgn-dash/src/server.ts
+++ apps/sgn-dash/src/server.ts
@@ -11,7 +11,7 @@ const nextApp = next({ dev: Bun.env.NODE_ENV !== "production" });
 const handle = nextApp.getRequestHandler();
 
 type Client = {
-  key: string | null;
+  isAdmin: boolean;
   socket: WebSocket;
 };
 const clients: Set<Client> = new Set();
@@ -25,13 +25,19 @@ nextApp.prepare().then(async () => {
 
   const wss = new WebSocketServer({ noServer: true });
 
-  wss.on("connection", (ws: WebSocket, req: IncomingMessage) => {
+  wss.on("connection", async (ws: WebSocket, req: IncomingMessage) => {
     const search = new URLSearchParams(req.url);
     const key =
       (req.headers["x-api-key"] as string | null) ??
       search.get("/api/ws?apikey");
+    const session = await auth.api.getSession({
+      headers: new Headers({
+        "x-api-key": key ?? "",
+      }),
+    });
+    const isAdmin = session?.user.tags.includes("admin") ?? false;
     const cl: Client = {
-      key: key,
+      isAdmin,
       socket: ws,
     };
     clients.add(cl);
@@ -59,15 +65,7 @@ nextApp.prepare().then(async () => {
   console.log("Server listening on port 3000");
 
   await pgClient.subscribe("*:stargates", (row, { command, relation }) => {
-    clients.forEach(async ({ socket, key }) => {
-      const session = await auth.api.getSession({
-        headers: new Headers({
-          "x-api-key": key ?? "",
-        }),
-      });
-
-      const isAdmin = session?.user.tags.includes("admin");
-
+    clients.forEach(async ({ socket, isAdmin }) => {
       if (isAdmin) {
         socket.send(
           JSON.stringify({
