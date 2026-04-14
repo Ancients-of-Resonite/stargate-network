13b98178d45a8a7c49522c0ff06b0a3b4ee8b32e
reality-exe <reality@uninetwork.net>
2026-04-13 20:02:05 -0400

    specify publication

--- packages/database/src/db.ts
+++ packages/database/src/db.ts
@@ -8,6 +8,9 @@ export { eq } from "drizzle-orm";
 
 export const pgClient = postgres(
   `postgresql://${Bun.env.DB_USER}:${Bun.env.DB_PASS}@${Bun.env.DB_HOST}:${Bun.env.DB_PORT ?? 5432}/${Bun.env.DB_DATABASE}`,
+  {
+    publications: "alltables",
+  },
 );
 
 export const db = drizzle(pgClient, {
